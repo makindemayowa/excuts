9cb1408da7d2fd913c7ff3e99da52c9c6e178c13
Mayowa Makinde <mayowa.makinde@andela.com>
2018-05-17 15:26:41 +0100

    setup redux on client side

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import logo from './logo.svg';
 import './App.css';
+import { simpleAction } from './actions/simpleAction';
+
 
 class App extends Component {
+
   render() {
     return (
       <div className="App">
@@ -11,11 +15,24 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          To get started, edit <code>src/App.js</code> and save to reload
         </p>
+        <button onClick={() => this.props.simpleAction()}>Test redux action</button>
+        <pre>
+          {
+            JSON.stringify(this.props)
+          }
+        </pre>
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = state => ({
+  ...state
+});
+const mapDispatchToProps = dispatch => ({
+  simpleAction: () => dispatch(simpleAction())
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,24 @@
+import 'isomorphic-fetch';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'isomorphic-fetch';
-import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux'
+import store from './store';
 import './index.css';
 import App from './App';
+import registerServiceWorker from './registerServiceWorker';
 
 const url = '/api/user';
 
-fetch(url)
-  .then((resp) => resp.json()) // Transform the data into json
-  .then(function (data) {
-    console.log(data)
-  })
+// fetch(url)
+//   .then((resp) => resp.json()) // Transform the data into json
+//   .then(function (data) {
+//     console.log(data)
+//   })
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store()}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
