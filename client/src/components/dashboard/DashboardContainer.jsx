a5c47bbb9a5962ce00cc6fff9f9421c42b0d0589
Mayowa Makinde <mankinde53@yahoo.com>
2018-06-20 01:08:03 +0100

    djfnfj

--- client/src/components/dashboard/DashboardContainer.jsx
+++ client/src/components/dashboard/DashboardContainer.jsx
@@ -5,7 +5,6 @@ import { connect } from 'react-redux';
 import { simpleAction } from '../../actions/simpleAction';
 import NavBar from '../common/Nav';
 import Footer from '../common/Footer';
-// import SubNav from '../subNav/SubNav';
 
 class App extends Component {
   render() {
